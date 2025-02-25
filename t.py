e3580e5f2c5ff4d01aba3356358bae9d33a67496
Daniel Manascurta <daniel.manascurta@diconiumcom>
2025-02-25 21:13:21 +0200

    tests

--- t.py
+++ t.py
@@ -1,8 +1,13 @@
 # from datetime import datetime
 import datetime
+from pathlib import Path
 
 # start_time1 = datetime.now()
-start_time2 = datetime.datetime.now()
+# start_time2 = datetime.datetime.now()
 
 # print(start_time1)
-print(start_time2)
+# print(start_time2)
+
+ING = Path("testdir")
+ING.mkdir(pa)
+ING.mkdir(parents=True, exist_ok=True)
